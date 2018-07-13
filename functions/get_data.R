0ff90c9769404ffdb7d0b7a51600cf33e6e4a66a
Josh Jacobson <joshuah.jacobson@gmail.com>
2018-07-13 14:53:58 -0600

    setup data processing on remote server

--- functions/get_data.R
+++ functions/get_data.R
@@ -2,12 +2,16 @@
 
 ## Function to build a list of ensemble data frames
 
+## Local
+# source("~/GitHub/random-fields/functions/build_ensemble.R")
+
+## Remote
+source("build_ensemble.R")
+
 get_data <- function(samp_size, range, n=11) {
   
   # samp_size: number of realizations to be simulated 
   # ...: parameters passed to build_ensemble()
-  
-  source("~/GitHub/random-fields/functions/build_ensemble.R")
   
   ## collect each ensemble in a list
   data <- list()
@@ -22,6 +26,3 @@ get_data <- function(samp_size, range, n=11) {
   
 }
 
-# fields_data_s46_n5 <- get_data(5, c(4,6))
-# save(fields_data_ds_xi075_n11,
-#      file = "~/GitHub/random-fields/data/fields_data_ds_xi075_n11.RData")
--- simulate_realizations.R
+++ simulate_realizations.R
@@ -1,16 +1,28 @@
 
 ## Simulate 1K realizations for a variety of range values
 
+## Local 
+# source("~/GitHub/random-fields/functions/get_data.R")
+
+## Remote
+source("get_data.R")
+
 s_1 <- 4
 s_2 <- seq(1, 6, 0.5)
-s_2 <-s_2[s_2 != 4] # don't need to look at same scale
+#s_2 <-s_2[s_2 != 4] # don't need to look at same scale
 
 for (ii in s_2) {
-  paste("range param: ", ii)
+  print(paste("range param: ", ii))
   fields_data <- get_data(1000, c(s_1,ii))
   nam <- paste("fields_data_s4", ii, sep = "")
+  
+  ## Local
+  # save(fields_data,
+  #      file = paste("~/GitHub/random-fields/data/fields_data_s",
+  #                   s_1, ii, ".RData", sep = ""))
+  
+  ## Remote
   save(fields_data,
-       file = paste("~/GitHub/random-fields/data/fields_data_s",
-                    s_1, ii, ".RData", sep = ""))
+       file = paste("fields_data_s", s_1, ii, ".RData", sep = ""))
   
 }
