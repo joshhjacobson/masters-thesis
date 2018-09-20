b11b23cdb42617dc88b7cbb293e8b9c27f6143a2
Josh Jacobson <joshuah.jacobson@gmail.com>
2018-09-20 13:55:14 -0600

    optimize simulation with fewer root-finding computations

--- simulation_standalone.R
+++ simulation_standalone.R
@@ -15,32 +15,31 @@ rho_root <- function(rho, xi, smooth, rng, var) {
 rhored_search <- function(xi, smooth, rng, var) {
   # xi (float): desired weight ratio between ensemble mean and perturbation
   # NOTE: other model parameters passed to RMbiwm() are assumed to be set and constant
-  rhored <- uniroot(rho_root, c(xi, 1), xi=xi, smooth=smooth, rng=rng, var=var)$root
-  return (rhored)
+  if(rng[1]==rng[3]){
+    return(xi)
+  } else{
+    rhored <- uniroot(rho_root, c(xi, 1), xi=xi, smooth=smooth, rng=rng, var=var)$root
+    return (rhored)
+  }
 }
 
 
 ## Function to construct single realization
-build_ensemble <- function(range, xi=0.8, x=NULL, y=NULL, n=11) {
+build_ensemble <- function(rng, rhored, xi=0.8, smooth=c(1.5, 1.5, 1.5), var=c(1, 1),
+                           x=NULL, y=NULL, n=11) {
   
-  # range (list of 2): scale parameters for observation and ensemble; c(s_1, s_2)
-  # xi (float): desired weight ratio between ensemble mean and perturbation
+  # rng (list of 3): scale parameters for observation and ensemble; c(s_1, gmean, s_2), s=1/a
+  # rho: percent cross correlation 
+  # xi (float): weight ratio between ensemble mean and perturbation
+  # smooth: smoothnes / differentiability (nu)
+  # var: variances
   # x, y (arrays): field grid points
   # n (num): number of ensemble members
-  
-  s_1 <- range[1]
-  s_2 <- range[2]
   
   if (is.null(x) | is.null(y)) {
     x <- y <- seq(-20, 20, 0.2)
   }
-  
-  ## parameters
-  smooth <- c(1.5, 1.5, 1.5)                     #nu: smoothnes / differentiability
-  rng <- c(s_1, sqrt(s_1*s_2), s_2)              #range: s = 1/a  
-  var <- c(1, 1)                                 #variances
-  rhored <- rhored_search(xi, smooth, rng, var)  #rho: percent cross correlation 
-  
+   
   ## model
   model_biwm <- RMbiwm(nu = smooth, s = rng, cdiag = var, rhored = rhored)
   fields <- RFsimulate(model_biwm, x, y)
@@ -74,17 +73,25 @@ build_ensemble <- function(range, xi=0.8, x=NULL, y=NULL, n=11) {
 
 
 ## Function to build a list of ensemble data frames
-get_data <- function(samp_size, range, xi=0.8, n=11) {
+get_data <- function(samp_size, range, xi=0.8, smooth=c(1.5, 1.5, 1.5), var=c(1, 1), n=11) {
   
   # samp_size: number of realizations to be simulated 
   # ...: parameters passed to build_ensemble()
+  
+  s_1 <- range[1]
+  s_2 <- range[2]
+  rng <- c(s_1, sqrt(s_1*s_2), s_2)  #geometric mean
+  
+  ## determine rho for desired xi at given range
+  rhored <- rhored_search(xi, smooth, rng, var)
+  print(rhored)
   
   ## collect realizations in a list
   data <- list()
   
   for (i in 1:samp_size) {
     print(i)
-    fields <- build_ensemble(range=range, xi=xi, n=n)
+    fields <- build_ensemble(rng=rng, rhored=rhored, xi=xi, n=n)
     data[[i]] <- fields
   }
   
@@ -107,9 +114,9 @@ for (ii in s_2) {
   fields_data <- get_data(N, c(s_1,ii))
   
   ## Local
-  save(fields_data,
-       file = paste("~/GitHub/random-fields/data/fields/",
-                    nam, ".RData", sep = ""))
+  # save(fields_data,
+  #      file = paste("~/GitHub/random-fields/data/",
+  #                   nam, ".RData", sep = ""))
   
   ## Remote
   # save(fields_data,
