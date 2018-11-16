506203c3d14bfceb3f13e19a2565ee0d8e852601
Josh Jacobson <joshuah.jacobson@gmail.com>
2018-11-16 16:14:27 -0700

    improve simulation memory allocation

--- tinker-scripts/simulate_s1_old.R
+++ tinker-scripts/simulate_s1_old.R
@@ -24,23 +24,11 @@ rhored_search <- function(xi, smooth, rng, var) {
 }
 
 
-## Function to obtain the threshhold exeedence for each field in a realization
-exceedence <- function(data, tau) {
-  # data: observation and ensemble in cols of data frame
-  # tau: threshold
-  m <- array(colMeans(data > tau))
-  return(m)
-}
-
-
 ## Function to construct single realization
-build_ensemble <- function(arr_dat, index, rng, rhored, 
-                           xi=0.8, smooth=c(1.5, 1.5, 1.5), var=c(1, 1), n=11) {
+build_ensemble <- function(rng, rhored, xi=0.8, smooth=c(1.5, 1.5, 1.5), var=c(1, 1), n=11) {
   
-  # arr_dat (array): 4D array storing exceedence data
-  # index: realization number and s2 id
   # rng (list of 3): scale parameters for observation and ensemble; c(s_1, gmean, s_2), s=1/a
-  # rhored: percent cross correlation 
+  # rho: percent cross correlation 
   # xi (float): weight ratio between ensemble mean and perturbation
   # smooth: smoothnes or differentiability (nu)
   # var: variances
@@ -61,6 +49,7 @@ build_ensemble <- function(arr_dat, index, rng, rhored,
   omega <- RFsimulate(model_whittle, x, y, n=n)
   omega <- as.matrix(data.frame(omega))
   
+  
   ensemble_mean <- fields$variable2
   ensemble_mean <- replicate(n, ensemble_mean)
   
@@ -69,69 +58,83 @@ build_ensemble <- function(arr_dat, index, rng, rhored,
   
   ensemble <- xi*ensemble_mean + sqrt(1-xi^2)*omega
   
+  
   ## realization
   realization <- data.frame(fields$variable1, ensemble)
   # names(realization) <- c("obs", paste("f", 1:n, sep = ""))
   
-  ## compute field exceedences
-  tau <- seq(0, 4, 0.5)
-  for(t in 1:length(tau)) {
-    arr_dat[index[1],,t,index[2]] <- exceedence(realization, tau[t])
+  return(realization)
+}
+
+
+## Function to build a list of ensemble data frames
+get_data <- function(samp_size, range, xi=0.8, smooth=c(1.5, 1.5, 1.5), var=c(1, 1), n=11) {
+  
+  # samp_size: number of realizations to be simulated 
+  # ...: parameters passed to build_ensemble()
+  
+  s_1 <- range[1]
+  s_2 <- range[2]
+  rng <- c(s_1, sqrt(s_1*s_2), s_2)  #geometric mean
+  
+  ## determine rho for desired xi at given range
+  rhored <- rhored_search(xi, smooth, rng, var)
+  
+  ## collect realizations in a list
+  data <- list()
+
+  for (i in 1:samp_size) {
+    print(i)
+    fields <- build_ensemble(rng=rng, rhored=rhored, xi=xi, n=n)
+    data[[i]] <- fields
   }
-  
-  ## save array slice dim(ens-mem x tau) into 4D array
-  nam <- paste("exceed_dat_s", rng[1], sep="")
-  save(arr_dat, file = paste(nam, ".RData", sep = ""))
-  
-  return(arr_dat)
+
+  return(data)
+}
+
+
+## Function to obtain the threshhold exeedence for each field in a realization
+exceedence <- function(data, tau) {
+  # data: observation and ensemble in cols of data frame
+  # tau: threshold
+  m <- array(colMeans(data > tau))
+  return(m)
 }
 
 
 ## Function to simulate N realizations for each (s_1,s_2) pair 
 ## where s_1 is given and s_2 in in seq(0.5*s_1, 1.5*s_1, 0.1*s_1)
-range_sim <- function(s_1, N) {
+range_sim <- function(s_1, N, fname) {
   
   # s_1 (num): observation range parameter (s=1/a)
   # N (num): number of samples to generate for each (s_1, s_2) pair
+  # fname: string like "rank_dat_sX"; indicates which s1 array is being built
   
   s_2 <- seq(0.5*s_1, 1.5*s_1, 0.1*s_1)
+  tau <- seq(0, 4, 0.5)
   
-  ## load or build 4D array
-  init_array <- function(N, s_2) {
-    ## create array for each s_1 dim(N x realization member x tau x s2)
-    arr_dat <- array(dim = c(N, 12, 9, length(s_2)))
-    return(arr_dat)
-  }
-  ## check whether data already exists
-  check <- try(load(paste("exceed_dat_s", s_1, ".RData", sep="")))
-  if("try-error" %in% class(check)) {
-    arr_dat <- init_array(N, s_2)
-  }
+  ## create array for each s_1 dim(N x realization member x tau x s2)
+  arr_dat <- array(dim = c(N, 12, length(tau), length(s_2)))
   
   ## simulations
   for (ii in 1:length(s_2)) {
-    set.seed(20*s_1+ii-1)
     print(paste("range:", s_1, s_2[ii], sep=" "))
+    fields_list <- get_data(N, c(s_1, s_2[ii]))
     
-    ## check if current s_2 is already completely filled out
-    if ( is.na(arr_dat[N, 12, 9, ii]) ) {
-      xi <- 0.8; smooth <- c(1.5, 1.5, 1.5); var <- c(1, 1); n=11
-      rng <- c(s_1, sqrt(s_1*s_2[ii]), s_2[ii])  #geometric mean
-      
-      ## determine rho for desired xi at given range
-      rhored <- rhored_search(xi, smooth, rng, var)
-      
-      ## loop over N (sample size)
-      for (jj in 1:length(arr_dat[,1,1,1])) {
-        print(jj)
-        arr_dat <- build_ensemble(arr_dat, index=c(jj,ii), rng=rng, rhored=rhored, xi=xi, n=n)
-      }
-    } else {
-      print(paste("Data found: skipping s_2 = ", s_2[ii], sep=""))
+    ## compute field exceedences
+    for(t in 1:length(tau)) {
+      arr_dat[,,t,ii] <- t(sapply(fields_list, exceedence, tau=tau[t]))
     }
+    
+    ## overwrite results after each s2 param completed
+    ## NOTE: change file name if restarting mid sim
+    ## (convert 4D array to rectangular format)
+    print("saving...")
+    save(arr_dat, file = paste(fname, ".RData", sep = ""))
+    
   }
   
-  return("Complete")
+  return(arr_dat)
 }
 
 
@@ -139,9 +142,12 @@ range_sim <- function(s_1, N) {
 # Simulation script -------------------------------------------------------
 
 ## Simulate N realizations for a variety of range values
+set.seed(10)
 s_1 <- 1
 N <- 4
+nam <- paste("exceed_dat_s", s_1, sep="")
 
-range_sim(s_1, N)
+
+arr_dat <- range_sim(s_1, N, nam)
 
 
