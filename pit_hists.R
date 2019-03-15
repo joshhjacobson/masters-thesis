54cb0fa3d23ced895baedea8fa515a9925a896c7
Josh Jacobson <joshuah.jacobson@gmail.com>
2019-03-14 20:40:45 -0600

    pit hists

--- pit_hists.R
+++ pit_hists.R
@@ -64,7 +64,7 @@ rm(ranks_arr, ranks_df)
 # }
 
 ## iterate over time, compute pointwise ranks across members
-ranks_arr <- apply(field_dat[1:4,1:3,1:12,], c(3,1,2), 
+ranks_arr <- apply(field_dat, c(3,1,2), 
                    function(point){
                      if(length(unique(point)) == 1) { 
                        return(NA)
@@ -86,22 +86,27 @@ rand_count_df <- data.frame(date=dates, rand_arr) %>%
   mutate(m = month(date)) %>%
   select(-date) %>%
   group_by(m) %>%
-  summarise(r_percent = sum(count)/sum(total))
+  summarise(r_percent = sum(count)/sum(total) * 100)
 
 ranks_df <- melt(ranks_df, variable.name='date', value.name='rank')
-# levels(i$Species) <- c("S", "Ve", "Vi")
+
+## create labels for facets
+rp <- c(
+  '1' = paste('Jan, ', round(rand_count_df[1,2], 2), '%', sep=''),
+  '4' = paste('Apr, ', round(rand_count_df[2,2], 2), '%', sep=''),
+  '7' = paste('Jul, ', round(rand_count_df[3,2], 2), '%', sep=''),
+  '10' = paste('Oct, ', round(rand_count_df[4,2], 2), '%', sep='')
+)
 
 ## stratify days by month and build histograms
 pdf("~/GitHub/random-fields/images/pit_downscaled_wot_hists.pdf",
     width = 10, height = 4)
 ranks_df %>% 
   mutate(month = month(date)) %>%
-  mutate(month = paste(month,', ',rand_count_df[m==month]$r_percent,'%',sep=''))
-
   ggplot(aes(x=rank)) +
   geom_histogram(binwidth = 1, fill="steelblue", color="white", size=0.25) +
   scale_x_continuous(breaks=seq(0,12,4)) +
-  facet_wrap(~month, ncol=4) +
+  facet_wrap(~month, ncol=4, labeller=as_labeller(rp)) +
   labs(title='Standard PIT histogram: downscaled forecasts, full-ties removed')
 dev.off()
 
