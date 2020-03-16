9b7dc31f3c367808f42f62bc7c786f15ef347d6c
Josh Jacobson <joshuah.jacobson@gmail.com>
2020-03-15 20:29:00 -0600

    Add beta params to new figs

--- figs/hist_supplement.R
+++ figs/hist_supplement.R
@@ -1,11 +1,17 @@
 
 library(tidyverse)
+library(fitdistrplus)
 library(latex2exp)
 library(grid)
 library(gridExtra)
 
 # Get ranks data
 rank_tab <- read.table('../data/rank_tab.RData')
+
+disagg_rank <- function(r) {
+  return(runif(1, r-1/24, r+1/24))
+}
+
 
 
 # five histograms per ratio (0.5, 1.1?): fix ratio, plot hist for threshold in (0,1,2,3,4), include percent dropped (or at least number)
@@ -16,11 +22,6 @@ tau_labs <- c(
   '3' = TeX(paste("$\\tau = 3$")),
   '4' = TeX(paste("$\\tau = 4$"))
 )
-
-# ratio_labs <- c(
-#   '0.5' = "ratio = 0.5",
-#   '1.1' = "ratio = 1.1"
-# )
 
 
 df <- rank_tab %>%
@@ -35,6 +36,15 @@ sum(is.na(df_tau4$rank)) # 1996 nans
 levels(df$tau) <- tau_labs
 # levels(df$ratio) <- ratio
 
+params <- df %>%
+  drop_na() %>%
+  mutate(rank = sapply(rank, disagg_rank)) %>%
+  group_by(tau) %>%
+  summarise(params=paste(fitdist(rank,'beta')$estimate, collapse=" ")) %>%
+  separate(params, c('a', 'b'), sep=" ") %>%
+  mutate(a=round(as.numeric(a), 3), b=round(as.numeric(b), 3)) %>%
+  unite(params, a:b, sep = ", ")
+
 
 png("hists_over_tau_r05.png", units="in", height=1.8, width=8, res=300, pointsize=10)
 df %>%
@@ -42,6 +52,7 @@ df %>%
     geom_hline(yintercept=1, linetype=3, size=0.3, color="grey") +
     geom_histogram(aes(y=..density..), bins=12, fill="black", color="white") +
     facet_wrap(~tau, nrow=1, labeller=label_parsed) +
+    annotate("text", x=0.48, y=3.8, size=4, label=params$params) +
     labs(y="", x="") +
     theme_bw() +
     theme(legend.title = element_blank(),
@@ -73,13 +84,24 @@ sum(is.na(df_tau4$rank)) # 3990 nans
 
 levels(df$tau) <- tau_labs
 
-# 1996 nans
+params <- df %>%
+  drop_na() %>%
+  mutate(rank = sapply(rank, disagg_rank)) %>%
+  group_by(tau) %>%
+  summarise(params=paste(fitdist(rank,'beta')$estimate, collapse=" ")) %>%
+  separate(params, c('a', 'b'), sep=" ") %>%
+  mutate(a=round(as.numeric(a), 3), b=round(as.numeric(b), 3)) %>%
+  unite(params, a:b, sep = ", ")
+
+
 png("hists_over_tau_r11.png", units="in", height=1.8, width=8, res=300, pointsize=10)
 df %>%
   ggplot(aes(x=rank)) +
   geom_hline(yintercept=1, linetype=3, size=0.3, color="grey") +
   geom_histogram(aes(y=..density..), bins=12, fill="black", color="white") +
   facet_wrap(~tau, nrow=1, labeller=label_parsed) +
+  annotate("text", x=0.48, y=1.3, size=4, label=params$params) +
+  ylim(0, 1.4) +
   labs(y="", x="") +
   theme_bw() +
   theme(legend.title = element_blank(),
@@ -110,6 +132,18 @@ ratio_labs <- c(
   '1.5' = "ratio = 1.5"
 )
 
+params <- rank_tab %>% 
+  dplyr::filter(s1==1 & s2 %in% am_vec & tau==1) %>%
+  mutate(rank = (rank-0.5)/12) %>%
+  drop_na() %>%
+  mutate(rank = sapply(rank, disagg_rank)) %>%
+  group_by(s2) %>%
+  summarise(params=paste(fitdist(rank,'beta')$estimate, collapse=" ")) %>%
+  separate(params, c('a', 'b'), sep=" ") %>%
+  mutate(a=round(as.numeric(a), 3), b=round(as.numeric(b), 3)) %>%
+  unite(params, a:b, sep = ", ")
+
+
 png("hists_over_ratio.png", units="in", height=1.8, width=8, res=300, pointsize=10)
 
 rank_tab %>% 
@@ -119,6 +153,7 @@ rank_tab %>%
     geom_hline(yintercept=1, linetype=3, size=0.3, color="grey") +
     geom_histogram(aes(y=..density..), bins=12, fill="black", color="white") +
     facet_wrap(~s2, nrow=1, labeller=as_labeller(ratio_labs)) +
+    annotate("text", x=0.48, y=3, size=4, label=params$params) +
     labs(y="", x="") +
     theme_bw() +
     theme(legend.title = element_blank(),
